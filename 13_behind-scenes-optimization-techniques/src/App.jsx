921b2a25cfbebab8a38b37e92e4277bf5cdb6fc4
christoxl <kzms664@astrazeneca.net>
2024-07-01 16:19:17 -0600

    behind the scenes and optimization techniques

---
+++ 13_behind-scenes-optimization-techniques/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+import Counter from "./components/Counter/Counter";
+import Header from "./components/Header";
+import { log } from "./log.js";
+import ConfigureCounter from "./components/Counter/ConfigureCounter";
+
+function App() {
+  log("<App /> rendered");
+
+  const [chosenCount, setChosenCount] = useState(0);
+
+  function handleSetCount(newCount) {
+    setChosenCount(newCount);
+  }
+
+  return (
+    <>
+      <Header />
+      <main>
+        <ConfigureCounter onSet={handleSetCount} />
+        <Counter key={chosenCount} initialCount={chosenCount} />
+      </main>
+    </>
+  );
+}
+
+export default App;
